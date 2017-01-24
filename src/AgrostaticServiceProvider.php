4d73d72d042e6fe7e4d93219502e03a58a3f336d
Yuriy Maslov <yuriy.maslof@gmail.com>
2017-01-24 14:10:29 +0300

    some test fixes

--- src/AgrostaticServiceProvider.php
+++ src/AgrostaticServiceProvider.php
@@ -6,18 +6,6 @@ use Illuminate\Support\ServiceProvider;
 
 class AgrostaticServiceProvider extends ServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
-
-
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
     public function boot()
     {
         $this->publishes([__DIR__ . '/../config/' => config_path() . "/"], 'config');
