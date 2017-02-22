94037573f4eaab539b4aac0882ed6e124e025e4f
FairHypo <yuriy.maslof@gmail.com>
2017-02-22 16:01:04 +0300

    before new branch

--- helpers/Agro.php
+++ helpers/Agro.php
@@ -1,4 +1,25 @@
 <?php
+function getServicesToken($grant_type = 'client_credentials', $client_id = null, $client_secret = null, $scope = null)
+{
+    if( empty($client_id) || empty($client_secret) ) {
+        $client_id = env('STATIC_CLIENT_ID');
+        $client_secret = env('STATIC_CLIENT_SECRET');
+    }
+
+    $guzzle = new GuzzleHttp\Client;
+
+    $response = $guzzle->post(env('STATIC_HOST') . '/oauth/token', [
+        'form_params' => [
+            'grant_type' => $grant_type,
+            'client_id' => $client_id,
+            'client_secret' => $client_secret,
+            'scope' => $scope,
+        ],
+    ]);
+
+    return $response;
+}
+
 function getStaticPath( $identifier = null, $width = null, $type = null )
 {
     if( empty($type) ) {
@@ -37,5 +58,5 @@ function getStaticPath( $identifier = null, $width = null, $type = null )
     if( $file != false && $file->path[0] != '/' ) {
         $file->path = '/' . $file->path;
     }
-    return !empty($file) ? env('STATIC_BASE_URL') . '/' . env('STATIC_IMG_ROOT')  . $file->path . '/' . $file->name : false;
+    return !empty($file) ? env('STATIC_HOST') . '/' . env('STATIC_IMG_ROOT')  . $file->path . '/' . $file->name : false;
 }
